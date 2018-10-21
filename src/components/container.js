e88ac4fc28752aa1d238e244f3f77b72677bb60b
hrryndrsn <harryanderson9@gmail.com>
2018-10-21 15:49:55 +1100

    add points by clicking path

--- src/components/container.js
+++ src/components/container.js
@@ -1,106 +1,109 @@
-import React from 'react';
-import './container.css';
-import Point from './point'
+import React from "react";
+import "./container.css";
+import Point from "./point";
 
 export default class Container extends React.Component {
-  
   constructor(props) {
-      super(props);
-      this.state = { 
-        x: 0,
-        y: 0,
-        h: 0,
-        w: 0,
-        cx: 0,
-        cy: 0,
-        curvePct: 0.0,
-        points: [],
-        selectedPoint: 0,
-        uid: 2,
-        isDragging: false,
-        activePoint: 0,
-      };
+    super(props);
+    this.state = {
+      x: 0,
+      y: 0,
+      h: 0,
+      w: 0,
+      cx: 0,
+      cy: 0,
+      curvePct: 0.0,
+      points: [],
+      selectedPoint: 0,
+      uid: 2,
+      isDragging: false,
+      activePoint: 0
+    };
 
-      this.containerRef = React.createRef();
-      this.pathRef = React.createRef();
-      this.updateDimensions = this.updateDimensions.bind(this);
+    this.containerRef = React.createRef();
+    this.pathRef = React.createRef();
+    this.updateDimensions = this.updateDimensions.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({
+      h: window.innerHeight,
+      w: window.innerWidth
+    });
+    window.addEventListener("resize", this.updateDimensions);
+  }
+
+  updateDimensions() {
+    this.setState({
+      h: window.innerHeight,
+      w: window.innerWidth
+    });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateDimensions);
+  }
+
+  pointOnCrv(pct) {
+    var length = this.pathRef.current.getTotalLength();
+    var containerWidth = this.state.w * 0.8;
+    var offset = (this.state.w - this.state.w * 0.8) / 2;
+    var relPct = ((this.state.x - offset) / containerWidth) * length;
+    var crds = this.pathRef.current.getPointAtLength(relPct);
+    return crds;
+  }
+
+  _onMouseMove(e) {
+    //find point on the curve
+    var length = this.pathRef.current.getTotalLength();
+    var offset = (this.state.w - this.state.w * 0.8) / 2;
+    var containerWidth = this.state.w * 0.8;
+    var pct = Math.floor(((e.screenX - offset) / containerWidth) * 100) / 100;
+    var relPct = ((e.screenX - offset) / containerWidth) * length;
+    var crds = this.pathRef.current.getPointAtLength(relPct);
+    var selectedPoint;
+
+    if (e.target.id === "") {
+      selectedPoint = 0;
+    } else {
+      selectedPoint = parseInt(e.target.id);
     }
+    this.setState({
+      x: e.screenX,
+      y: e.screenY,
+      cx: crds.x,
+      cy: crds.y,
+      curvePct: pct,
+      selectedPoint
+    });
+  }
 
-    componentDidMount() {
-      this.setState({
-        h: window.innerHeight,
-        w: window.innerWidth,
-      })
-      window.addEventListener("resize", this.updateDimensions);
-    }
-
-    updateDimensions() {
-      this.setState({
-        h: window.innerHeight, 
-        w: window.innerWidth
+  handleMouseDown(e) {
+    console.log(e.target == this.pathRef.current)
+    let activePoint = parseInt(e.target.id);
+    this.setState({ isDragging: true, activePoint: activePoint });
+    if (this.state.selectedPoint === 0 && e.target == this.pathRef.current) {
+      this.state.points.push({
+        name: "heuy",
+        id: this.state.uid,
+        x: this.state.cx,
+        y: this.state.cy
       });
+      var i = this.state.uid + 1;
+      this.setState({ uid: i });
+      return;
     }
+  }
 
-    componentWillUnmount() {
-      window.removeEventListener("resize", this.updateDimensions);
-    }
+  handleMouseUp(e) {
+    this.setState({ isDragging: false, activePoint: 0 });
+  }
 
-    pointOnCrv(pct) {
-      var length = this.pathRef.current.getTotalLength();
-      var containerWidth = this.state.w * 0.8
-      var offset = (this.state.w - (this.state.w * 0.8)) / 2
-      var relPct = ((this.state.x - offset)/containerWidth) * length
-      var crds = this.pathRef.current.getPointAtLength(relPct)
-      return crds;
-    }
-
-  
-    _onMouseMove(e) {
-      //find point on the curve
-      var length = this.pathRef.current.getTotalLength();
-      var offset = (this.state.w - (this.state.w * 0.8)) / 2
-      var containerWidth = this.state.w * 0.8
-      var pct = Math.floor(((e.screenX - offset)/containerWidth)*100) / 100
-      var relPct = ((e.screenX - offset)/containerWidth) * length
-      var crds = this.pathRef.current.getPointAtLength(relPct)
-      var selectedPoint;
-
-      if (e.target.id === "") {
-        selectedPoint = 0
-      } else {
-        selectedPoint = parseInt(e.target.id)
-      }
-      
-      this.setState({ 
-        x: e.screenX, 
-        y: e.screenY,  
-        cx: crds.x, 
-        cy: crds.y, 
-        curvePct: pct,
-        selectedPoint,
-      }); 
-    }
-
-    handleMouseDown(e) {
-      let activePoint = parseInt(e.target.id);
-      this.setState({isDragging: true, activePoint: activePoint});
-      if (this.state.selectedPoint === 0) {
-        this.state.points.push({name: 'heuy', id: this.state.uid, x: this.state.cx, y: this.state.cy})
-        var i = this.state.uid + 1
-        this.setState({uid: i})
-        return
-      } 
-    }
-
-    handleMouseUp(e) {
-      this.setState({isDragging: false, activePoint: 0});
-    }
-
-    renderPoint(point) {      
-      return (
-      <Point 
-        key={point.id} 
-        x={point.x} 
+  renderPoint(point) {
+    return (
+      <Point
+        key={point.id}
+        x={point.x}
         y={point.y}
         id={point.id}
         cx={this.state.cx}
@@ -108,38 +111,32 @@ export default class Container extends React.Component {
         isDragging={this.state.isDragging}
         activePoint={this.state.activePoint}
         name={point.name}
-        />)
-    }
-
-  
-    render() {      
-      return (
-      
-      <div 
-        className="container" 
-        ref={this.containerRef} 
-        onMouseDown={this.handleMouseDown.bind(this)} 
-        onMouseUp={this.handleMouseUp.bind(this)}
-        onMouseMove={this._onMouseMove.bind(this)}
-        >
-        <div id="obj"></div>
-        <svg width="100%" height="63%" viewBox="0 0 100 63" fill="none">
-              
-              <path 
-                d="M5 43.9307C27.3438 43.9307 27.5 16 50 16C72.5 16 72.6562 43.9307 95 43.9307" 
-                stroke="black" 
-                strokeWidth="1"
-                ref={this.pathRef}
-              />
-              {
-                this.state.points.map(point => this.renderPoint(point))
-              }
-        </svg>
-
-      </div>)
-    }
+      />
+    );
   }
 
+  render() {
+    return (
+      <div
+        className="container"
+        ref={this.containerRef}
+        onMouseDown={this.handleMouseDown.bind(this)}
+        onMouseUp={this.handleMouseUp.bind(this)}
+        onMouseMove={this._onMouseMove.bind(this)}
+      >
+        <div id="obj" />
+        <svg width="100%" height="63%" viewBox="0 0 100 63" fill="none">
+          <path
+            d="M5 43.9307C27.3438 43.9307 27.5 16 50 16C72.5 16 72.6562 43.9307 95 43.9307"
+            stroke="black"
+            strokeWidth="1"
+            ref={this.pathRef}
+          />
+          {this.state.points.map(point => this.renderPoint(point))}
+        </svg>
 
-
-
+        <input type="text" value="seefef"/>
+      </div>
+    );
+  }
+}
--- src/components/point.js
+++ src/components/point.js
@@ -1,90 +1,99 @@
-import React from 'react'
-import "./point.css"
+import React from "react";
+import "./point.css";
 
 export default class Container extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            color: "#000",
-            x: this.props.x,
-            y: this.props.y,
-            cx: this.props.cx,
-            cy: this.props.cy,
-            id: this.props.id,
-            isDragging: false,
-            selected: false,
-            r: "2.5"
-        }
-    }
-    
-    selected() {
-        console.log("selected point: ", this.state.id)
-        this.setState({
-            selected: true
-        })
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: "#000",
+      x: this.props.x,
+      y: this.props.y,
+      cx: this.props.cx,
+      cy: this.props.cy,
+      id: this.props.id,
+      isDragging: false,
+      selected: false,
+      r: "2.5"
+    };
+  }
 
-    deselected() {
-        this.setState({
-            selected: false,
-        })
-    }
+  selected() {
+    console.log("selected point: ", this.state.id);
+    this.setState({
+      selected: true
+    });
+  }
 
-    handleMouseDown = (e) => {
-        this.cords = {
-            x: e.pageX,
-            y: e.pageY,
-            isDragging: e.target.id === this.state.id,
-        }
-        console.log(e.target.id === this.state.id)
-        document.addEventListener('mousemove', this.handleMouseMove);
-    }
+  deselected() {
+    this.setState({
+      selected: false
+    });
+  }
 
-    handleMouseMove = (e) => {
-        this.setState({
-            x: this.props.cx, 
-            y: this.props.cy,
-        })
-    }
+  handleMouseDown = e => {
+    this.cords = {
+      x: e.pageX,
+      y: e.pageY,
+      isDragging: e.target.id === this.state.id
+    };
+    console.log(e.target.id === this.state.id);
+    document.addEventListener("mousemove", this.handleMouseMove);
+  };
 
-    handleMouseUp = () => {
-        document.removeEventListener('mousemove', this.handleMouseMove);
-    }
+  handleMouseMove = e => {
+    this.setState({
+      x: this.props.cx,
+      y: this.props.cy
+    });
+  };
 
-    renderClassList = () => {
-        if (this.props.activePoint === this.state.id) {
-            return "active"
-        } else {
-            return "point"
-        }
-    }
+  handleMouseUp = () => {
+    document.removeEventListener("mousemove", this.handleMouseMove);
+  };
 
-    render() {
-        const {x, y, id, color, r} = this.state
-        const xOffset = 5
-        const yOffset = 5
-        if (!(this.props.isDragging)) {
-            document.removeEventListener('mousemove', this.handleMouseMove);
-        } 
-        return (
-        <svg>
-        <text x={this.state.x - xOffset} y={this.state.y - yOffset} className="pointName" fill={color}>{this.props.name}</text>
-        <circle 
-            cx={x} 
-            cy={y}
-            key={id} 
-            r={r}
-            fill={color}
-            stroke="#fff"
-            strokeWidth="1"
-            onMouseEnter={this.selected.bind(this)}
-            onMouseLeave={this.deselected.bind(this)}
-            onMouseDown={this.handleMouseDown}
-            onMouseUp={this.handleMouseUp}
-            id={id}
-            className={this.props.activePoint === this.state.id ? "active point" : "point"}
-            />
-        </svg>
-        )
+  renderClassList = () => {
+    if (this.props.activePoint === this.state.id) {
+      return "active";
+    } else {
+      return "point";
     }
+  };
+
+  render() {
+    const { x, y, id, color, r } = this.state;
+    const xOffset = 5;
+    const yOffset = 5;
+    if (!this.props.isDragging) {
+      document.removeEventListener("mousemove", this.handleMouseMove);
+    }
+    return (
+      <svg>
+        <text
+          x={this.state.x - xOffset}
+          y={this.state.y - yOffset}
+          className="pointName"
+          fill={color}
+        >
+          {this.props.name}
+        </text>
+        <circle
+          cx={x}
+          cy={y}
+          key={id}
+          r={r}
+          fill={color}
+          stroke="#fff"
+          strokeWidth="1"
+          onMouseEnter={this.selected.bind(this)}
+          onMouseLeave={this.deselected.bind(this)}
+          onMouseDown={this.handleMouseDown}
+          onMouseUp={this.handleMouseUp}
+          id={id}
+          className={
+            this.props.activePoint === this.state.id ? "active point" : "point"
+          }
+        />
+      </svg>
+    );
+  }
 }
