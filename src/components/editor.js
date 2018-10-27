99f6ca286644f9abd76cb72a970b99eb7bbfdd30
hrryndrsn <harryanderson9@gmail.com>
2018-10-27 14:44:27 +1100

    styling changes

--- src/components/editor.js
+++ src/components/editor.js
@@ -2,46 +2,61 @@ import React from "react";
 import styled from "styled-components";
 
 const EditorContainer = styled.div`
-    width: 100%;
-    height: 50px;
-    position: absolute;
-    top: -50px;
-    left: 0;
-    display: flex;
-    justify-content: center;
-    background: ${props => props.selectedPoint ? "white": "none"};
-    transition: 0.2s ease-in;
-    font-size: 2em;
-    & input {
-        font-size: 1em;
-        border: none;
-        border-radius: 5px;
-        &:focus {
-            box-shadow: none;
-            outline: none;
-        }
+  width: 100%;
+  height: 50px;
+  position: absolute;
+  top: -50px;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  background: ${props => (props.selectedPoint ? "white" : "none")};
+  transition: 0.2s ease-in;
+  font-size: 2em;
+  & input {
+    font-size: 1em;
+    border: none;
+    border-radius: 5px;
+    &:focus {
+      box-shadow: none;
+      outline: none;
     }
-    &.active {
-        top: 0px;
-    }
-`
+  }
+  &.active {
+    top: 0px;
+  }
+`;
 
+const LabelDiv = styled.div`
+  height: 100%;
+  line-height: 50px;
+  font-size: 0.5em;
+  margin-right: 20px;
+  color: rgba(0, 0, 0, 0.4);
+  padding-top: 3px;
+  display: ${props => (props.selectedPoint ? "white" : "none")};
+`;
 
 export default class Container extends React.Component {
   render() {
     return (
-        <EditorContainer selectedPoint={this.props.selectedPoint} className={this.props.selectedPoint ? "active" : ""}>
-            <input
-                type="text"
-                id="nameInput"
-                value={this.props.name}
-                onChange={this.props.onChange}
-                autoFocus={true}
-                style={{
-                display: this.props.selectedPoint ? "initial" : "none"
-                }}
-            />
-        </EditorContainer>
+      <EditorContainer
+        selectedPoint={this.props.selectedPoint}
+        className={this.props.selectedPoint ? "active" : ""}
+      >
+        <LabelDiv className="noselect" selectedPoint={this.props.selectedPoint}>
+          Label
+        </LabelDiv>
+        <input
+          type="text"
+          id="nameInput"
+          value={this.props.name}
+          onChange={this.props.onChange}
+          autoFocus={true}
+          style={{
+            display: this.props.selectedPoint ? "initial" : "none"
+          }}
+        />
+      </EditorContainer>
     );
   }
 }
